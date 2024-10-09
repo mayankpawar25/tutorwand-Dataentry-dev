166e579e98e85949ddf67288a92a9c22c65b9985
Mayank <mayank.pawar@webcraftit.com>
2024-10-09 18:32:19 +0530

    Stop tracking storage directory

--- storage/framework/views/57672c4a7c918c09491dca90d2aadcc4f380cb2e.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    let ajaxPaginationUrl = "<?php echo e(route('ajax.pagination')); ?>";
-    let spinner = `<span class="spinner-border spinner-border-sm"></span> Update`;
-    let saveTxt = "Update";
-    let selectFilters = "Select all filters";
-</script><?php /**PATH /home1/abhyur2a/public_html/tutorwand.com/dataentry-dev/resources/views/admin/layout/partials/reviewScript.blade.php ENDPATH**/ ?>
--- storage/framework/views/6d834650bf666b88ba39cc179c4cf4e5ae4290f7.php
+++
@@ -1,6 +0,0 @@
-<?php if(isset($response)): ?>
-    <option value=""><?php echo e($label); ?></option>
-    <?php $__currentLoopData = $response; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $resp): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <option value="<?php echo e($resp['id']); ?>"><?php echo e($resp['name']); ?></option>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php endif; ?><?php /**PATH /home1/abhyur2a/public_html/tutorwand.com/dataentry-dev/resources/views/admin/selectbox.blade.php ENDPATH**/ ?>
